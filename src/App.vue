97e20e9e21bd4649ff0ec3d7ab053871ba342a59
yeyoung8805 <yyk8805@naver.com>
2022-08-30 15:59:12 +0900

    apply <RouterView />

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,4 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <RouterView />
 </template>
-
-<script>
-import HelloWorld from '~/components/HelloWorld'
-
-export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
-    }
-  }
-}
-</script>
+>
