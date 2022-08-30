f8409d68adf002194d37ab80fd1f5c255917aa4f
yeyoung8805 <yyk8805@naver.com>
2022-08-30 16:28:45 +0900

    add Header.vue to make Navigations

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="nav">
+      <div class="nav-item">
+        <RouterLink class="nav-link"> About </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Search',
+          href='/',
+        },
+        {
+          name: 'Movie',
+          href='/movie',
+        },
+        {
+          name: 'About',
+          href='/about',
+        }
+      ]
+    }
+  }
+}
+</script>
