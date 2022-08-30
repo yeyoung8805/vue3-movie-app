6c49f620855b74895aab7bfd766e8e86fd3edb88
yeyoung8805 <yyk8805@naver.com>
2022-08-30 17:56:01 +0900

    add center style to logo, and nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,3 +38,15 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center; /* 가운데 정렬 */
+  .logo {
+    margin-right: 40px; /* header 안에 있을 경우만 margin-right 40px 준다. */
+  }
+}
+</style>
