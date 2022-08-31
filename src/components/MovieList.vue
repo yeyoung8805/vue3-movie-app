50afaf0c5b428dbd206188f91d7caee099f2a373
yeyoung8805 <yyk8805@naver.com>
2022-08-31 13:57:13 +0900

    create MovieList, MovieItem

---
+++ src/components/MovieList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <MovieItem />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieItem from "~/components/MovieItem.vue";
+
+export default {
+  components: {
+    MovieItem,
+  },
+  data() {
+    return {
+      movies: [],
+    };
+  },
+};
+</script>
