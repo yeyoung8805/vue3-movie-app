f0b8134444e4ede96ed0d80b123d6142f03088dc
yeyoung8805 <yyk8805@naver.com>
2022-08-31 09:27:14 +0900

    add data to Search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -14,6 +14,23 @@ export default {
   data() {
     return {
       title: "",
+      type: "movie",
+      number: 10,
+      year: "",
+      filters: [
+        {
+          name: "type",
+          items: ["movie", "series", "episode"],
+        },
+        {
+          name: "number",
+          items: [10, 20, 30],
+        },
+        {
+          name: "year",
+          items: [2022, 2021, 2020, 2019, 2018, 2017],
+        },
+      ],
     };
   },
 };
