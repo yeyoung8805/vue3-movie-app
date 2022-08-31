8ee5fd4c6450e73882dbfce2865810509f91c530
yeyoung8805 <yyk8805@naver.com>
2022-08-31 16:05:24 +0900

    return originally

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="inner">
-      <MovieItem v-for="hello in movies" :key="hello.imdbID" :movie="hello" />
+      <MovieItem v-for="movie in movies" :key="movie.imdbID" :movie="movie" />
     </div>
   </div>
 </template>
