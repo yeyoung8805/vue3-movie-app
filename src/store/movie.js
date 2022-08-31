11db6449c86c8d985e4d122f00f1c30557bc9e1d
yeyoung8805 <yyk8805@naver.com>
2022-08-31 14:29:43 +0900

    mutations, actions are methods but different

--- src/store/movie.js
+++ src/store/movie.js
@@ -12,6 +12,18 @@ export default {
     },
   },
   //methods 와 유사한 mutations, actions
-  mutations: "",
-  actions: "",
+  //mutations : 변이. state의 data는 mutations 에서만 수정하는게 허용되며 getters, actions 에서 수정하는게 허용되지 않는다.
+  mutations: {
+    resetMovies(state) {
+      state.movies = [];
+    },
+  },
+  //actions : 비동기로 작동한다.
+  actions: {
+    searchMovies(context) {
+      context.state;
+      context.getters;
+      context.commit;
+    },
+  },
 };
