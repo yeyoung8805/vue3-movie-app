ce27c811f3d8edc5a03d39ad249a18a5dda3ac33
yeyoung8805 <yyk8805@naver.com>
2022-08-31 16:09:12 +0900

    replace to arrow function

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -7,11 +7,7 @@ export default {
   props: {
     movie: {
       type: Object,
-      // 화살표 함수 형식. 아래와 같은 내용이다.
-      // dafault: () => ({}),
-      default: function () {
-        return {};
-      },
+      dafault: () => ({}),
     },
   },
 };
