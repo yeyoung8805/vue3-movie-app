d15963e295569433f7ced909e3750f526c4b6e0f
yeyoung8805 <yyk8805@naver.com>
2022-08-31 14:19:31 +0900

    add comment to attributes in movie.js

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,7 +1,11 @@
 export default {
-  namespaced: "",
+  //module로 활용할수 있다는 뜻
+  namespaced: true,
+  //취급해야 하는 data
   state: "",
+  //computed 계산된 데이터를 만들어내는 상태
   getters: "",
+  //methods 와 유사한 mutations, actions
   mutations: "",
   actions: "",
 };
