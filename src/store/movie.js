17452a4039b2cfdd921cb4e3ff91af1f4a88efdf
yeyoung8805 <yyk8805@naver.com>
2022-08-31 14:22:08 +0900

    edit return -> to {} datas

--- src/store/movie.js
+++ src/store/movie.js
@@ -2,11 +2,9 @@ export default {
   //module로 활용할수 있다는 뜻
   namespaced: true,
   //취급해야 하는 data
-  state: () => {
-    return {
-      movies: [],
-    };
-  },
+  state: () => ({
+    movies: [],
+  }),
   //computed 계산된 데이터를 만들어내는 상태
   getters: "",
   //methods 와 유사한 mutations, actions
