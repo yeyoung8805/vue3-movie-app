723fae2a48fd3acf19d38b50f190b4f5489325f7
yeyoung8805 <yyk8805@naver.com>
2022-08-31 14:20:32 +0900

    add function to state

--- src/store/movie.js
+++ src/store/movie.js
@@ -2,7 +2,11 @@ export default {
   //module로 활용할수 있다는 뜻
   namespaced: true,
   //취급해야 하는 data
-  state: "",
+  state: function () {
+    return {
+      movies: [],
+    };
+  },
   //computed 계산된 데이터를 만들어내는 상태
   getters: "",
   //methods 와 유사한 mutations, actions
