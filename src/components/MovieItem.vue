42ef0141459e0e3dadbab697d6a2903b240a334c
yeyoung8805 <yyk8805@naver.com>
2022-08-31 16:08:32 +0900

    add props movie to MovieItem.vue

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,1 +1,18 @@
-<template></template>
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object,
+      // 화살표 함수 형식. 아래와 같은 내용이다.
+      // dafault: () => ({}),
+      default: function () {
+        return {};
+      },
+    },
+  },
+};
+</script>
